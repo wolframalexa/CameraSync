306f55d0b3db75fa965f44396a82728dc7046774
wolframalexa <alexajakob@tutanota.com>
2020-12-16 11:10:49 -0500

    edit cap _> camera

--- opentwo.py
+++ opentwo.py
@@ -10,8 +10,8 @@ if not (camera2.isOpened()):
 	print("Could not open camera 2")
 
 #To get the resolution
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(camera1.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(camera1.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # set up writer
 writer1 = cv2.VideoWriter('samplevideocamera1.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width,height))
