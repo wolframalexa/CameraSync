ce82046afbd68ffe06db0260f784b2b1739bcc0e
wolframalexa <alexajakob@tutanota.com>
2021-01-27 11:44:23 -0500

    separate RGB & depth

---
+++ getdepth.cpp
@@ -0,0 +1,31 @@
+#include <k4a/k4a.h>
+
+k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+config.camera_fps = K4A_FRAMES_PER_SECOND_30;
+config.color_format = K4A_IMAGE_FORMAT_COLOR_MJPG;
+config.color_resolution = K4A_COLOR_RESOLUTION_2160P;
+config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
+
+if (K4A_RESULT_SUCCEEDED != k4a_device_start_cameras(device, &config))
+{
+    printf("Failed to start device\n");
+    goto Exit;
+}
+
+// Capture a depth frame
+switch (k4a_device_get_capture(device, &capture, TIMEOUT_IN_MS))
+{
+case K4A_WAIT_RESULT_SUCCEEDED:
+    break;
+case K4A_WAIT_RESULT_TIMEOUT:
+    printf("Timed out waiting for a capture\n");
+    continue;
+    break;
+case K4A_WAIT_RESULT_FAILED:
+    printf("Failed to read a capture\n");
+    goto Exit;
+}
+
+// get depth image from capture
+k4a_capture_get_depth_image(capture, capture_handle)
+
