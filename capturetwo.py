3b4ee252db288a931bd723d3f93d40f4677fbf0c
Alexa Jakob <alexajakob@tutanota.com>
2021-03-31 15:59:08 -0400

    run tests 5, 6, 7

--- capturetwo.py
+++ capturetwo.py
@@ -7,7 +7,7 @@ def get_time():
 
 
 #imagesFolder = "~/Documents/AutonomyLab/CameraSync/captures"
-timestamps = open("004_timedata.txt",'a')
+timestamps = open("009_timedata.txt",'a')
 
 # open device
 camera1 = cv2.VideoCapture('v4l2src device=/dev/video0 io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
@@ -33,33 +33,33 @@ print("Frame rate for camera 2:",frameRate2)
 
 
 # set up writer
-writer1 = cv2.VideoWriter('004_video1.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width,height))
-writer2 = cv2.VideoWriter('004_video2.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width,height))
+writer1 = cv2.VideoWriter('009_video1.mp4v', cv2.VideoWriter_fourcc(*'hevc'), 30, (width,height))
+writer2 = cv2.VideoWriter('009_video2.mp4v', cv2.VideoWriter_fourcc(*'hevc'), 30, (width,height))
 # DIVX is the video codec, works on all platforms
 
 while(True):
 	# Capture frame-by-frame
 	ret1, frame1 = camera1.read()
 	timestamps.write(str(get_time()) + "\n")
-	writer1.write(frame1)
+#	writer1.write(frame1)
 
 	ret2, frame2 = camera2.read()
 	timestamps.write(str(get_time()) + 2 * "\n")
-	writer2.write(frame2)
+#	writer2.write(frame2)
 
 	# Display the resulting frame
-	cv2.imshow('preview1',frame1)
-	cv2.imshow('preview2',frame2)
+#	cv2.imshow('preview1',frame1)
+#	cv2.imshow('preview2',frame2)
 
 	# Capture frames every second
-	frameId = camera1.get(1) # current frame number
-	if (frameId % int(frameRate1) == 0):
-		filename1 =  "captures/004_image1_" + str(int(frameId)) + ".jpg"
-		status1 = cv2.imwrite(filename1, frame1)
+#	frameId = camera1.get(1) # current frame number
+#	if (frameId % int(frameRate1) == 0):
+#		filename1 =  "captures/004_image1_" + str(int(frameId)) + ".jpg"
+#		status1 = cv2.imwrite(filename1, frame1)
 #		timestamps.write(str(get_time()) + "\n")
 
-		filename2 =  "captures/004_image2_" + str(int(frameId)) + ".jpg"
-		status2 = cv2.imwrite(filename2, frame2)
+#		filename2 =  "captures/004_image2_" + str(int(frameId)) + ".jpg"
+#		status2 = cv2.imwrite(filename2, frame2)
 #		timestamps.write(str(get_time()) + 2 * "\n")
 
 
@@ -71,8 +71,8 @@ while(True):
 camera1.release()
 camera2.release()
 
-writer1.release()
-writer2.release()
+#writer1.release()
+#writer2.release()
 
 cv2.destroyAllWindows()
 timestamps.close()
