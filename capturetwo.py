1c8717e4c908c0bd1d56144970888d582114daa1
wolframalexa <alexajakob@tutanota.com>
2021-04-07 10:55:56 -0400

    dynamic to static memory

--- capturetwo.py
+++ capturetwo.py
@@ -1,13 +1,17 @@
 import cv2
 import math
 import time
+import numpy as np
 
 def get_time():
         return round(time.time() * 1000)
 
 
 #imagesFolder = "~/Documents/AutonomyLab/CameraSync/captures"
-timestamps = open("009_timedata.txt",'a')
+
+n = 36000 # timestamp every 0.1 second for an hour
+time1 = np.zeroes((1,n))
+time2 = np.zeroes((1,n))
 
 # open device
 camera1 = cv2.VideoCapture('v4l2src device=/dev/video0 io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
@@ -22,52 +26,46 @@ if not (camera2.isOpened()):
 #To get the resolution
 width = int(camera1.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(camera1.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print("Dimensions:", width, height)
-
 
 # get frame rate
 frameRate1 = camera1.get(5)
-print("Frame rate for camera 1:", frameRate1)
 frameRate2 = camera2.get(5)
-print("Frame rate for camera 2:",frameRate2)
-
 
 # set up writer
 writer1 = cv2.VideoWriter('009_video1.mp4v', cv2.VideoWriter_fourcc(*'hevc'), 30, (width,height))
 writer2 = cv2.VideoWriter('009_video2.mp4v', cv2.VideoWriter_fourcc(*'hevc'), 30, (width,height))
-# DIVX is the video codec, works on all platforms
 
+i = 0
 while(True):
 	# Capture frame-by-frame
 	ret1, frame1 = camera1.read()
-	timestamps.write(str(get_time()) + "\n")
 #	writer1.write(frame1)
 
 	ret2, frame2 = camera2.read()
-	timestamps.write(str(get_time()) + 2 * "\n")
 #	writer2.write(frame2)
 
 	# Display the resulting frame
 #	cv2.imshow('preview1',frame1)
 #	cv2.imshow('preview2',frame2)
 
-	# Capture frames every second
-#	frameId = camera1.get(1) # current frame number
-#	if (frameId % int(frameRate1) == 0):
+	# Capture frames every tenth of a second
+	frameId1 = camera1.get(1) # current frame number
+	frameId2 = camera2.get(1)
+	if ((frameId1 * 10) % int(frameRate1) == 0):
 #		filename1 =  "captures/004_image1_" + str(int(frameId)) + ".jpg"
 #		status1 = cv2.imwrite(filename1, frame1)
-#		timestamps.write(str(get_time()) + "\n")
+		time1[i] = frameId1/frameRate1
 
 #		filename2 =  "captures/004_image2_" + str(int(frameId)) + ".jpg"
 #		status2 = cv2.imwrite(filename2, frame2)
-#		timestamps.write(str(get_time()) + 2 * "\n")
-
+		time2[i] = frameId2/frameRate2
+		i +=1
+		print(i)
 
 	#Waits for a user input to quit the application
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	if i == 3600 | (cv2.waitKey(1) & 0xFF == ord('q')):
 		break
 
-# When everything done, release the capture 
 camera1.release()
 camera2.release()
 
@@ -75,5 +73,3 @@ camera2.release()
 #writer2.release()
 
 cv2.destroyAllWindows()
-timestamps.close()
-
