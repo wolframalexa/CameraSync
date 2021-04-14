145bb217c092ad719329284a58ea1dac7032a985
wolframalexa <alexajakob@tutanota.com>
2021-04-14 12:58:33 -0400

    move towards multithreading

--- capturetwo.py
+++ capturetwo.py
@@ -1,79 +1,63 @@
 import cv2
+import threading
 import math
 import time
 import numpy as np
+
+
+class camThread(threading.Thread):
+	def __init__(self, previewName, camID):
+		threading.Thread.__init__(self)
+		self.previewName = previewName
+		self.camID = camID
+	def run(self, timearray):
+		print "Starting " + self.previewName
+		camPreview(self.previewName, self.camID, timearray)
+
+def camPreview(previewName, camID, timearray):
+	cv2.namedWindow(previewName)
+	cam = cv2.VideoCapture('v4l2src device=/dev/video' + str(camID) + ' io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
+	writer = cv2.VideoWriter('004_video1.mp4v', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width,height))
+	frameRate = cam.get(5)
+
+	if cam.isOpened(): #try to get first frame
+		rval, frame = cam.read()
+		starttime = time.time()
+	else:
+		rval = False
+
+	while rval:
+		cv2.imshow(previewName, frame)
+		rval, frame = cam.read()
+		writer.write(frame)
+
+		frameId = cam.get(1)
+		if (frameId % int(frameRate) == 0):
+			filename = "captures/004_image_" + str(int(frameId)) + ".jpg"
+			timearray[i] = time.time() - starttime
+			i += 1
+
+		if i == n | key == 27: # exit on ESC
+			endtime = time.time()
+			break
+	cam.release()
+	writer.release()
+	cv2.destroyWindow(previewName)
 
 n = 100
 time1 = np.zeros((n,1))
 time2 = np.zeros((n,1))
 
-# open device
-camera1 = cv2.VideoCapture('v4l2src device=/dev/video0 io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
-camera2 = cv2.VideoCapture('v4l2src device=/dev/video1 io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
+thread1 = camThread("Camera 1", 0, time1)
+thread2 = camThread("Camera 2", 1, time2)
 
-if not (camera1.isOpened()):
-	print("Could not open camera 1")
-if not (camera2.isOpened()):
-	print("Could not open camera 2")
-
-
-#To get the resolution
-width = int(camera1.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(camera1.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-# get frame rate
-frameRate1 = camera1.get(5)
-frameRate2 = camera2.get(5)
-
-# set up writer
-writer1 = cv2.VideoWriter('009_video1.mp4v', cv2.VideoWriter_fourcc(*'hevc'), 30, (width,height))
-writer2 = cv2.VideoWriter('009_video2.mp4v', cv2.VideoWriter_fourcc(*'hevc'), 30, (width,height))
-
-i = 0
 starttime = time.time()
-while(True):
-	# Capture frame-by-frame
-	ret1, frame1 = camera1.read()
-	time1[i] = time.time() - starttime
-#	writer1.write(frame1)
+thread1.start()
+thread2.start()
 
-	ret2, frame2 = camera2.read()
-	time2[i] = time.time() - starttime
-#	writer2.write(frame2)
 
-	# Display the resulting frame
-#	cv2.imshow('preview1',frame1)
-#	cv2.imshow('preview2',frame2)
-
-	# Capture frames every tenth of a second
-#	frameId1 = camera1.get(1) # current frame number
-#	frameId2 = camera2.get(1)
-#	if ((frameId1 * 10) % int(frameRate1) == 0):
-#		filename1 =  "captures/004_image1_" + str(int(frameId)) + ".jpg"
-#		status1 = cv2.imwrite(filename1, frame1)
-#		time1[i] = frameId1/frameRate1
-
-#		filename2 =  "captures/004_image2_" + str(int(frameId)) + ".jpg"
-#		status2 = cv2.imwrite(filename2, frame2)
-#		time2[i] = frameId2/frameRate2
-	i +=1
-
-	#Waits for a user input to quit the application
-	if i == n | (cv2.waitKey(1) & 0xFF == ord('q')):
-		endtime = time.time()
-		break
-
-camera1.release()
-camera2.release()
-
-#writer1.release()
-#writer2.release()
-
-cv2.destroyAllWindows()
-
+# write data
 f = open("001d_timedata.txt",'a')
 f.write(str(time1))
 f.write(str(time2))
-f.write("Start time: " + str(starttime))
-f.write("End time: " + str(endtime))
 f.close()
