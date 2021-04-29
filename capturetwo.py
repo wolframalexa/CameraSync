4e77369d4f2380e2b8e680f3e864dc200a1bba15
wolframalexa <alexajakob@tutanota.com>
2021-04-29 12:47:14 -0400

    commit local files from laptop

--- capturetwo.py
+++ capturetwo.py
@@ -16,29 +16,24 @@ class camThread(threading.Thread):
 		camPreview(self.previewName, self.camID, self.timearray)
 
 def camPreview(previewName, camID, timearray):
-	print("In function camPreview)
-	cv2.namedWindow(previewName)
+
 	cam = cv2.VideoCapture('v4l2src device=/dev/video' + str(camID) + ' io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
 	writer = cv2.VideoWriter('004_video1.mp4v', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width,height))
 	frameRate = cam.get(5)
-	print("Frame rate: ", frameRate)
 
-	if cam.isOpened(): #try to get first frame
-		rval, frame = cam.read()
-		starttime = time.time()
-	else:
-		rval = False
+	if not cam.isOpened():
+		print("Could not open camera")
 
-	while rval:
-		cv2.imshow(previewName, frame)
+	while True:
 		rval, frame = cam.read()
 		writer.write(frame)
+		cv2.imshow(previewName,frame)
 
 		frameId = cam.get(1)
 		if (frameId % int(frameRate) == 0):
 			filename = "captures/004_image_" + str(int(frameId)) + ".jpg"
+			cv2.imwrite(filename,frame)
 			timearray[i] = time.time() - starttime
-			print("Index of timearray: ", i)
 			i += 1
 
 		if i == n | key == 27: # exit on ESC
@@ -62,6 +57,6 @@ thread2.start()
 
 # write data
 f = open("004d_timedata.txt",'a')
-f.write(str(time1))
-f.write(str(time2))
+f.write(str(time1.timearray)
+f.write(str(time2.timearray))
 f.close()
