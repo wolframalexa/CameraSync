b90dd773db0e01dad7432768d20647fca5bdef56
Alexa Jakob <alexajakob@tutanota.com>
2021-05-05 12:21:34 -0400

    fixed functional issues

--- capturetwo.py
+++ capturetwo.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 import numpy as np
-import multiprocessing import Process
+from multiprocessing import Process
 
 
 def runCamera(camID, timearray):
@@ -9,11 +9,11 @@ def runCamera(camID, timearray):
 	height = 1080
 
 	cap = cv2.VideoCapture('v4l2src device=/dev/video' + str(camID) + ' io-mode=2 ! image/jpeg, width=(int)1920, height=(int)1080 ! jpegdec ! video/x-raw ! videoconvert ! video/x-raw,format=BGR ! appsink', cv2.CAP_GSTREAMER)
-	writer = cv2.VideoWriter('004_video' + str(camID) + '.mp4v', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height)
-	frameRate = cap.get(5)
+	writer = cv2.VideoWriter('004_video' + str(camID) + '.mp4v', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
 
-	if not cam.isOpened():
+	if not cap.isOpened():
 		print("Could not open camera")
+	frameRate = cap.get(5)
 
 	i = 0
 	while(True):
@@ -22,9 +22,9 @@ def runCamera(camID, timearray):
 
 		cv2.imshow("Camera 0",frame)
 
-		frameId = cam.get(1)
+		frameId = cap.get(1)
 		if (frameId % int(frameRate) == 0):
-			filename = "captures/004_image_" + str(camID) + frameId + ".jpg"
+			filename = "captures/004_image_" + str(camID) + str(int(frameId)) + ".jpg"
 			cv2.imwrite(filename,frame)
 			timearray[i] = time.time() - starttime
 			i += 1
@@ -35,7 +35,7 @@ def runCamera(camID, timearray):
 	cap.release()
 	writer.release()
 	cv2.destroyAllWindows()
-
+	return timearray
 
 n = 10
 time1 = np.zeros((n,1))
